a2ff53837a4232c1eef69e444d8e19e65cfc6e75
CrArenas <crarenasv@unal.edu.co>
2025-06-12 18:23:00 -0500

    Añadido Alex en el seeder

--- gateway/database/seeders/UserSeeder.php
+++ gateway/database/seeders/UserSeeder.php
@@ -23,14 +23,14 @@ class UserSeeder extends Seeder
             [
                 'role_id' => 2,
                 'name' => 'Santiago',
-                'email' => 'santi@gmail.com',
-                'password' => bcrypt('123456')
+                'email' => 'santiago@gmail.com',
+                'password' => bcrypt('1234')
             ],
             [
                 'role_id' => 2,
-                'name' => 'Laura',
-                'email' => 'laura@gmail.com',
-                'password' => bcrypt('1234567')
+                'name' => 'Alex',
+                'email' => 'alex@gmail.com',
+                'password' => bcrypt('1234')
             ],
             [
                 'role_id' => 2,
